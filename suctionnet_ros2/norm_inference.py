6b014e90cf750e9ab926ea82017a8526fb489ac5
Jinkai <jinkaiq@andrew.cmu.edu>
2024-09-21 23:00:03 -0400

    updating visualization

--- suctionnet_ros2/norm_inference.py
+++ suctionnet_ros2/norm_inference.py
@@ -33,11 +33,11 @@ class NormStdInferencer:
         # print('heatmap:', heatmap.shape)
         heatmap = F.conv2d(heatmap, kernel).squeeze().numpy()
 
-        suction_scores, idx0, idx1 = grid_sample(heatmap, down_rate=50, topk=10)
+        suction_scores, idx0, idx1 = grid_sample(heatmap, down_rate=10, topk=10)
         suction_directions = normals[idx0, idx1, :]
         suction_translations = point_cloud[idx0, idx1, :]
+        self.visualize_heatmap(heatmap, rgb_img, idx0, idx1)
         self.visualize_suctions(suction_directions, suction_translations)
-        self.visualize_heatmap(heatmap, rgb_img)
 
     def visualize_suctions(self, suction_directions, suction_translations):
         import matplotlib.pyplot as plt
@@ -58,7 +58,7 @@ class NormStdInferencer:
         ax.scatter(trans_x, trans_y, trans_z, color='blue', label='Translations')
 
         # Use the quiver function to plot the directions as vectors
-        ax.quiver(trans_x, trans_y, trans_z, dir_x, dir_y, dir_z, length=0.01, normalize=True, color='red', label='Directions')
+        ax.quiver(trans_x, trans_y, trans_z, dir_x, dir_y, dir_z, length=0.002, color='red', label='Directions')
 
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
@@ -66,10 +66,19 @@ class NormStdInferencer:
         ax.set_title('3D Suction Directions and Translations')
         ax.legend()
 
+        # Set equal aspect ratio
+        max_range = np.array([trans_x.max()-trans_x.min(), trans_y.max()-trans_y.min(), trans_z.max()-trans_z.min()]).max() / 2.0
+        mid_x = (trans_x.max()+trans_x.min()) * 0.5
+        mid_y = (trans_y.max()+trans_y.min()) * 0.5
+        mid_z = (trans_z.max()+trans_z.min()) * 0.5
+        ax.set_xlim(mid_x - max_range, mid_x + max_range)
+        ax.set_ylim(mid_y - max_range, mid_y + max_range)
+        ax.set_zlim(mid_z - max_range, mid_z + max_range)
+        
         plt.show()
 
 
-    def visualize_heatmap(self, heatmap, rgb_img):
+    def visualize_heatmap(self, heatmap, rgb_img, idx0, idx1):
         import matplotlib.pyplot as plt
         score_image = heatmap
         score_image *= 255
@@ -78,6 +87,10 @@ class NormStdInferencer:
         score_image = cv2.applyColorMap(score_image, cv2.COLORMAP_RAINBOW)
         rgb_image = 0.5 * rgb_img + 0.5 * score_image
         rgb_image = rgb_image.astype(np.uint8)
+
+        # Plot dots on the idx0, idx1 positions
+        for i in range(len(idx0)):
+            cv2.circle(rgb_image, (idx1[i], idx0[i]), 5, (0, 255, 0), -1)
         
         # Display the image using Matplotlib
         plt.imshow(rgb_image)
@@ -89,8 +102,9 @@ if __name__ == "__main__":
     inferencer = NormStdInferencer()
     depth_dir = "/home/jinkai/Downloads/data/depth_image/first_scene/frame0000.jpg"
     rgb_dir = "/home/jinkai/Downloads/data/color_image/first_scene/frame0000.jpg"
+    seg_dir = "/home/jinkai/Downloads/data/seg_masks/first_scene.png"
     rgb_img = cv2.imread(rgb_dir)
     depth_img = cv2.imread(depth_dir, cv2.IMREAD_GRAYSCALE).astype(np.float32) / 1000.0
-    seg_mask = np.ones_like(depth_img, dtype=bool)
+    seg_mask = cv2.imread(seg_dir, cv2.IMREAD_GRAYSCALE).astype(bool)
     
     inferencer.infer(rgb_img, depth_img, seg_mask)
